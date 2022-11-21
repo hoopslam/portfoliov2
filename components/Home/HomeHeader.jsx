019a1d027caea5b707095ac38e0ff4053d335203
David <hoopslam@gmail.com>
2022-11-21 17:18:58 +0900

    fix: improve image load handling

--- components/Home/HomeHeader.jsx
+++ components/Home/HomeHeader.jsx
@@ -1,256 +1,275 @@
-import styled from "styled-components";
-import { WHATIDO } from "../../util/constants";
-import Link from "next/link";
+import styled from 'styled-components';
+import { WHATIDO, PROFILE_IMGS } from '../../util/constants';
+import Link from 'next/link';
 
 const HomeHeader = ({ selectedTheme, themeHandler }) => {
+    const toggler = (direction = 'right') => {
+        if (!direction) return null;
+        if (direction === 'right') {
+            selectedTheme < 4
+                ? themeHandler(selectedTheme + 1)
+                : themeHandler(0);
+        }
+        if (direction === 'left') {
+            selectedTheme > 0
+                ? themeHandler(selectedTheme - 1)
+                : themeHandler(4);
+        }
+    };
 
-  const toggler = (direction = "right") => {
-    if (direction === "right") {
-      selectedTheme < 4 ? themeHandler(selectedTheme + 1) : themeHandler(0);
-    } else if (direction === "left") {
-      selectedTheme > 0 ? themeHandler(selectedTheme - 1) : themeHandler(4);
-    } else return null;
-  };
+    return (
+        <Container id='home'>
+            <AvatarContainer>
+                <Slider
+                    className='left'
+                    onClick={() => toggler('left')}
+                >
+                    <Arrow className='left'>{'<'}</Arrow>
+                </Slider>
+                <Slider
+                    className='right'
+                    onClick={() => toggler('right')}
+                >
+                    <Arrow className='right'>{'>'}</Arrow>
+                </Slider>
+                {PROFILE_IMGS.map((profile_image, i) => (
+                    <Avatar
+                        active={i === selectedTheme}
+                        key={i}
+                        src={profile_image}
+                        alt='David Simpsonized Profile Image'
+                        width='350'
+                        height='350'
+                    />
+                ))}
+            </AvatarContainer>
+            <TextContainer>
+                <Greeting>
+                    <p>Howdy!</p>
 
-  return (
-    <Container id="home">
-      <AvatarContainer>
-        <Slider className="left" onClick={() => toggler("left")}>
-          <Arrow className="left">{"<"}</Arrow>
-        </Slider>
-        <Slider className="right" onClick={() => toggler("right")}>
-          <Arrow className="right">{">"}</Arrow>
-        </Slider>
-        <Avatar
-          src={`/images/${selectedTheme}.png`}
-          alt="David Simpsonized Profile Image"
-          width="350"
-          height="350"
-        />
-      </AvatarContainer>
-      <TextContainer>
-        <Greeting>
-          <p>Howdy!</p>
+                    <p>I&apos;m</p>
 
-          <p>I&apos;m</p>
-
-          <p>
-            <span>David</span>
-          </p>
-        </Greeting>
-
-        <TextBlock>
-          <p>
-            I&apos;m a{" "}
-            <span onClick={() => toggler("right")}>
-              {WHATIDO[selectedTheme]}
-            </span>
-          </p>
-        </TextBlock>
-      </TextContainer>
-      <Link href="/#about" passHref>
-        <a >
-          <DownArrow aria-label="About Link">
-            <div className="arrowContainer">
-              <div className="arrow"></div>
-            </div>
-          </DownArrow>
-        </a>
-      </Link>
-    </Container>
-  );
+                    <p>
+                        <span>David</span>
+                    </p>
+                </Greeting>
+                <TextBlock>
+                    <p>
+                        I&apos;m a{' '}
+                        <span onClick={() => toggler('right')}>
+                            {WHATIDO[selectedTheme]}
+                        </span>
+                    </p>
+                </TextBlock>
+            </TextContainer>
+            <Link
+                href='/#about'
+                passHref
+            >
+                <a>
+                    <DownArrow aria-label='About Link'>
+                        <div className='arrowContainer'>
+                            <div className='arrow'></div>
+                        </div>
+                    </DownArrow>
+                </a>
+            </Link>
+        </Container>
+    );
 };
 
 export default HomeHeader;
 
 const Container = styled.header`
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  @media screen and (max-width: 990px) {
-    flex-direction: column;
-  }
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    @media screen and (max-width: 990px) {
+        flex-direction: column;
+    }
 `;
 
 const AvatarContainer = styled.div`
-  position: relative;
-  width: 350px;
-  height: 350px;
-  margin: 15px;
+    position: relative;
+    width: 350px;
+    height: 350px;
+    margin: 15px;
 
-  @media screen and (max-width: 990px) {
-    width: 100%;
-    max-width: 250px;
-    height: 250px;
-  }
+    @media screen and (max-width: 990px) {
+        width: 100%;
+        max-width: 250px;
+        height: 250px;
+    }
 `;
 
 const Avatar = styled.img`
-  border-radius: 50%;
-  width: 350px;
-  height: 350px;
-  box-shadow: 0 0 5px #333;
-  cursor: pointer;
-  aspect-ratio: 1 / 1;
-  @media screen and (max-width: 990px) {
-    width: 100%;
-    width: 250px;
-    height: 250px;
-  }
+    display: ${(props) => (props.active ? `` : `none`)};
+    border-radius: 50%;
+    width: 350px;
+    height: 350px;
+    box-shadow: 0 0 5px #333;
+    cursor: pointer;
+    aspect-ratio: 1 / 1;
+    @media screen and (max-width: 990px) {
+        width: 100%;
+        width: 250px;
+        height: 250px;
+    }
 `;
 
 const TextContainer = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 const Greeting = styled.div`
-  color: ${({ theme }) => theme.textColor};
-  font-size: 1.5rem;
-  margin: 20px;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  span {
-    color: ${({ theme }) =>
-      theme.primary === "#263238"
-        ? "#00ff37"
-        : theme.primary === "#d32f2f"
-        ? "#1944d2"
-        : theme.primary};
+    color: ${({ theme }) => theme.textColor};
+    font-size: 1.5rem;
+    margin: 20px;
     position: relative;
-    font-size: 3rem;
-    font-weight: bold;
-    font-family: "Poppins", sans-serif;
-  }
-  p {
-    margin: 3px;
-  }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    span {
+        color: ${({ theme }) =>
+            theme.primary === '#263238'
+                ? '#00ff37'
+                : theme.primary === '#d32f2f'
+                ? '#1944d2'
+                : theme.primary};
+        position: relative;
+        font-size: 3rem;
+        font-weight: bold;
+        font-family: 'Poppins', sans-serif;
+    }
+    p {
+        margin: 3px;
+    }
 `;
 
 const TextBlock = styled.div`
-  margin: 15px;
-  color: ${({ theme }) => theme.textColor};
-  font-size: 1.5rem;
-  position: relative;
+    margin: 15px;
+    color: ${({ theme }) => theme.textColor};
+    font-size: 1.5rem;
+    position: relative;
 
-  span {
-    color: ${({ theme }) =>
-      theme.primary === "#263238" ? "#00ff37" : theme.primary};
-    font-weight: bold;
-    border-bottom: 2px solid
-      ${({ theme }) => (theme.primary === "#263238" ? "#000" : theme.primary)};
-    &:hover {
-      cursor: pointer;
+    span {
+        color: ${({ theme }) =>
+            theme.primary === '#263238' ? '#00ff37' : theme.primary};
+        font-weight: bold;
+        border-bottom: 2px solid
+            ${({ theme }) =>
+                theme.primary === '#263238' ? '#000' : theme.primary};
+        &:hover {
+            cursor: pointer;
+        }
     }
-  }
 `;
 
 const DownArrow = styled.div`
-  .arrowContainer {
-    position: absolute;
-    left: 50%;
-    bottom: 0px;
-    transform: translate(-50%, -50%);
-    display: flex;
-    height: 100px;
-    justify-content: center;
-    align-items: center;
-    animation: bounce 1.5s ease infinite;
-
-    @keyframes bounce {
-      0% {
-        bottom: 20px;
-      }
-      50% {
-        bottom: 10px;
-      }
-      100% {
-        bottom: 20px;
-      }
-    }
-  }
-
-  @media screen and (max-width: 990px) {
     .arrowContainer {
-      height: 50px;
+        position: absolute;
+        left: 50%;
+        bottom: 0px;
+        transform: translate(-50%, -50%);
+        display: flex;
+        height: 100px;
+        justify-content: center;
+        align-items: center;
+        animation: bounce 1.5s ease infinite;
 
-      @keyframes bounce {
-        0% {
-          bottom: 5px;
+        @keyframes bounce {
+            0% {
+                bottom: 20px;
+            }
+            50% {
+                bottom: 10px;
+            }
+            100% {
+                bottom: 20px;
+            }
         }
-        50% {
-          bottom: 0px;
-        }
-        100% {
-          bottom: 5px;
-        }
-      }
     }
-  }
 
-  .arrow {
-    height: 50px;
-    width: 50px;
-    border-style: solid;
-    border-color: ${({ theme }) => theme.primary};
-    border-width: 0px 4px 4px 0px;
-    transform: rotate(45deg);
-    transition: border-width 150ms ease-in-out;
+    @media screen and (max-width: 990px) {
+        .arrowContainer {
+            height: 50px;
 
-    :hover {
-      cursor: pointer;
-      border-color: ${({ theme }) =>
-        theme.light === "#4f5b62" ? theme.dark : theme.light};
+            @keyframes bounce {
+                0% {
+                    bottom: 5px;
+                }
+                50% {
+                    bottom: 0px;
+                }
+                100% {
+                    bottom: 5px;
+                }
+            }
+        }
     }
-  }
+
+    .arrow {
+        height: 50px;
+        width: 50px;
+        border-style: solid;
+        border-color: ${({ theme }) => theme.primary};
+        border-width: 0px 4px 4px 0px;
+        transform: rotate(45deg);
+        transition: border-width 150ms ease-in-out;
+
+        :hover {
+            cursor: pointer;
+            border-color: ${({ theme }) =>
+                theme.light === '#4f5b62' ? theme.dark : theme.light};
+        }
+    }
 `;
 
 const Arrow = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 40px;
-  height: 40px;
-  top: 50%;
-  transform: translate(0, -50%);
-  z-index: 11;
-  cursor: pointer;
-  user-select: none;
-  font-size: 2.5rem;
-  border-radius: 50%;
-  color: rgba(255, 255, 255, 0.89);
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    top: 50%;
+    transform: translate(0, -50%);
+    z-index: 11;
+    cursor: pointer;
+    user-select: none;
+    font-size: 2.5rem;
+    border-radius: 50%;
+    color: rgba(255, 255, 255, 0.89);
 
-  &.left {
-    left: 5px;
-  }
+    &.left {
+        left: 5px;
+    }
 
-  &.right {
-    right: 5px;
-  }
+    &.right {
+        right: 5px;
+    }
 `;
 
 const Slider = styled.div`
-  position: absolute;
-  width: 50%;
-  height: 100%;
-  z-index: 10;
-  cursor: pointer;
-  user-select: none;
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    z-index: 10;
+    cursor: pointer;
+    user-select: none;
 
-  &.left {
-    left: 0;
-  }
+    &.left {
+        left: 0;
+    }
 
-  &.right {
-    right: 0;
-  }
+    &.right {
+        right: 0;
+    }
 `;
--- util/constants.js
+++ util/constants.js
@@ -1,68 +1,92 @@
-export const WHATIDO = ["Software Developer", "Cat Dad", "Basketball Fanatic", "Gamer", "Foodie"];
+export const WHATIDO = [
+    'Software Developer',
+    'Cat Dad',
+    'Basketball Fanatic',
+    'Gamer',
+    'Foodie',
+];
 
-export const COLORS = [{id: 0, hex: "#1976d2"}, {id: 1, hex: "#f57c00"}, {id: 2, hex: "#d32f2f"}, {id: 3, hex: "#263238"}, {id: 4, hex: "#00796b"}];
+export const COLORS = [
+    { id: 0, hex: '#1976d2' },
+    { id: 1, hex: '#f57c00' },
+    { id: 2, hex: '#d32f2f' },
+    { id: 3, hex: '#263238' },
+    { id: 4, hex: '#00796b' },
+];
+
+export const PROFILE_IMGS = [
+    `/images/0.png`,
+    `/images/1.png`,
+    `/images/2.png`,
+    `/images/3.png`,
+    `/images/4.png`,
+];
 
 export const PROJECTS = [
-	{
+    {
         id: 1,
-		title: "Geography Quiz App",
-		image: "/images/projects/geoquiz.jpg",
-		mobile: "/images/projects/geoquizmobile.jpg",
-		description: "A random geography quiz app built in Typescript + React with MobX for global state management. Test your world knowledge!",
-		tech: "Typescript, React, MobX, Styled Components, Opentdb API",
-		github: "https://github.com/hoopslam/geoquiz",
-		live: "https://hoopslam.github.io/geoquiz/",
-	},
-	{
-		id: 2,
-		title: "Covid-19 Tracker",
-		description:
-			"A worldwide Covid-19 tracker app using live data from John's Hopkins University and Worldometers. Fully intactive map and charts that dynamically render based on user input.",
-		tech: "React(CRA), disease.sh API, React-Leaflet, Chart-JS",
-		image: "/images/projects/covid-0.png",
-		mobile: "/images/projects/covidmobile.jpg",
-		github: "https://github.com/hoopslam/Covid-19-tracker",
-		live: "https://hoopslam.github.io/Covid-19-tracker/",
-	},
-	{
-		id: 3,
-		title: "Artist Portfolio Website",
-		description:
-			"A fully responsive portfolio website with a custom-built modal for easy viewing and navigation.  Built with Next.js for search engine optimization, static site generation, and lazy loading features.",
-		tech: "React (Next.js), CSS Modules, Vercel",
-		image: "/images/projects/jiseon-0.png",
-		mobile: "/images/projects/jiseonmobile.jpg",
-		github: "https://github.com/hoopslam/lee_jiseon",
-		live: "https://www.jiseonlee.art/",
-	},
+        title: 'Geography Quiz App',
+        image: '/images/projects/geoquiz.jpg',
+        mobile: '/images/projects/geoquizmobile.jpg',
+        description:
+            'A random geography quiz app built in Typescript + React with MobX for global state management. Test your world knowledge!',
+        tech: 'Typescript, React, MobX, Styled Components, Opentdb API',
+        github: 'https://github.com/hoopslam/geoquiz',
+        live: 'https://hoopslam.github.io/geoquiz/',
+    },
+    {
+        id: 2,
+        title: 'Covid-19 Tracker',
+        description:
+            "A worldwide Covid-19 tracker app using live data from John's Hopkins University and Worldometers. Fully intactive map and charts that dynamically render based on user input.",
+        tech: 'React(CRA), disease.sh API, React-Leaflet, Chart-JS',
+        image: '/images/projects/covid-0.png',
+        mobile: '/images/projects/covidmobile.jpg',
+        github: 'https://github.com/hoopslam/Covid-19-tracker',
+        live: 'https://hoopslam.github.io/Covid-19-tracker/',
+    },
+    {
+        id: 3,
+        title: 'Artist Portfolio Website',
+        description:
+            'A fully responsive portfolio website with a custom-built modal for easy viewing and navigation.  Built with Next.js for search engine optimization, static site generation, and lazy loading features.',
+        tech: 'React (Next.js), CSS Modules, Vercel',
+        image: '/images/projects/jiseon-0.png',
+        mobile: '/images/projects/jiseonmobile.jpg',
+        github: 'https://github.com/hoopslam/lee_jiseon',
+        live: 'https://www.jiseonlee.art/',
+    },
     {
         id: 4,
-        title: "Tier List Maker",
-        image: "/images/projects/tier.jpg",
-		mobile: "/images/projects/tierlistmobile.jpg",
-        description: "A Tier List Maker built purely with vanilla HTML, CSS, and Javascript.  Drag and drop functionality using the native data-transfer API",
-        tech: "HTML, CSS, Javascript",
-        github: "https://github.com/hoopslam/tier-list-v1",
-        live: "https://hoopslam.github.io/tier-list-v1/"
+        title: 'Tier List Maker',
+        image: '/images/projects/tier.jpg',
+        mobile: '/images/projects/tierlistmobile.jpg',
+        description:
+            'A Tier List Maker built purely with vanilla HTML, CSS, and Javascript.  Drag and drop functionality using the native data-transfer API',
+        tech: 'HTML, CSS, Javascript',
+        github: 'https://github.com/hoopslam/tier-list-v1',
+        live: 'https://hoopslam.github.io/tier-list-v1/',
     },
     {
-        title: "Disney Plus Clone",
+        title: 'Disney Plus Clone',
         id: 5,
-        image: "/images/projects/disneyplus1.png",
-		mobile: "/images/projects/disneyplusmobile.jpg",
-        description: "A replica of the Disney Plus Front End.  React + Redux + Styled Components frontend and Firebase backend for authentication and database.",
-        tech: "React, Redux, Styled Components, Firebase",
-        github: "https://github.com/hoopslam/disneyplusclone",
-		live: "https://disneyplus-clone-703c8.web.app/",
+        image: '/images/projects/disneyplus1.png',
+        mobile: '/images/projects/disneyplusmobile.jpg',
+        description:
+            'A replica of the Disney Plus Front End.  React + Redux + Styled Components frontend and Firebase backend for authentication and database.',
+        tech: 'React, Redux, Styled Components, Firebase',
+        github: 'https://github.com/hoopslam/disneyplusclone',
+        live: 'https://disneyplus-clone-703c8.web.app/',
     },
-	{
-		title: "My Portfolio Website",
-		id: 6,
-		image: "/images/projects/newportfolio.png",
-		mobile: "/images/projects/newportfoliomobile.png",
-		description: "Curious how I made this site?  Feel free to have a look at my code or send me a message with any questions about the site.",
-		tech: "Next.js, Styled-Components, React-reveal",
-		github: "https://github.com/hoopslam/portfoliov2",
-		live: "https://devcho.com",
-	}
+    {
+        title: 'My Portfolio Website',
+        id: 6,
+        image: '/images/projects/newportfolio.png',
+        mobile: '/images/projects/newportfoliomobile.png',
+        description:
+            'Curious how I made this site?  Feel free to have a look at my code or send me a message with any questions about the site.',
+        tech: 'Next.js, Styled-Components, React-reveal',
+        github: 'https://github.com/hoopslam/portfoliov2',
+        live: 'https://devcho.com',
+    },
 ];
