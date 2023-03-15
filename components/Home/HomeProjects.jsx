2c55c8f745c4b8bccb0d78b7405754b9fed8b931
David <hoopslam@gmail.com>
2023-03-16 03:14:34 +0900

    chore: update links

--- components/Home/HomeProjects.jsx
+++ components/Home/HomeProjects.jsx
@@ -31,10 +31,6 @@ const ProjectsSectionContainer = styled.div`
     flex-direction: column;
     align-items: center;
 
-    /* * {
-        outline: 1px solid red;
-    } */
-
     h1 {
         color: ${({ theme }) => theme.primary};
         text-align: center;
@@ -61,86 +57,5 @@ const ProjectsContainer = styled.section`
     @media screen and (max-width: 767px) {
         grid-template-columns: 1fr;
         gap: 40px;
-    }
-`;
-
-const Project = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    margin: 10px 0;
-    box-shadow: 0 1px 5px #333;
-    border-radius: 12px;
-    background-color: ${({ theme }) =>
-        theme.primary === '#00ff37' ? '#000' : ''};
-
-    img {
-        width: 350px;
-        height: auto;
-        object-fit: cover;
-    }
-
-    @media screen and (max-width: 990px) {
-        flex-direction: column;
-        align-items: center;
-        max-width: 540px;
-
-        img {
-            width: 100%;
-            max-width: 450px;
-        }
-    }
-`;
-
-const LeftContainer = styled.div`
-    display: flex;
-    position: relative;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    min-width: 350px;
-
-    @media screen and (max-width: 990px) {
-        min-width: 0;
-    }
-
-    h2 {
-        font-size: 1.2rem;
-        text-align: center;
-        margin: 10px;
-    }
-`;
-
-const ImageContainer = styled.div`
-    box-shadow: 0 0 5px #333;
-    margin: 12px;
-`;
-
-const ProjectDescription = styled.div`
-    padding: 2.8rem 1.5rem 0;
-    display: flex;
-    flex-direction: column;
-
-    h3 {
-        font-size: 1rem;
-        margin: 10px 0;
-    }
-
-    h3 span {
-        color: ${({ theme }) => theme.primary};
-    }
-`;
-
-const Links = styled.div`
-    width: 100%;
-    display: flex;
-    padding-top: 10px;
-    justify-content: space-around;
-    align-items: center;
-
-    a {
-        padding: 10px;
-        color: ${({ theme }) => theme.primary};
-        text-decoration: none;
     }
 `;
